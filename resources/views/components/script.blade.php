ac6cb3a9c411b6f5f9c806a3578a808b2de85b1e
monzuralam <monzur707@gmail.com>
2024-05-01 13:03:08 +0600

    calendar markup init

--- resources/views/components/script.blade.php
+++ resources/views/components/script.blade.php
@@ -1,3 +1,4 @@
+
 <!-- JAVASCRIPT -->
 <script src={{ asset('assets/libs/jquery/jquery.min.js') }}></script>
 <script src={{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}></script>
@@ -5,6 +6,10 @@
 <script src={{ asset('assets/libs/simplebar/simplebar.min.js') }}></script>
 <script src={{ asset('assets/libs/node-waves/waves.min.js') }}></script>
 <script src={{ asset('assets/libs/feather-icons/feather.min.js') }}></script>
+<!-- calendar min js -->
+<script src={{ asset('assets/libs/fullcalendar/index.global.min.js') }}></script>
+<!-- Calendar init -->
+<script src={{ asset('assets/js/pages/calendar.init.js') }}></script>
 <!-- pace js -->
 <script src={{ asset('assets/libs/pace-js/pace.min.js') }}></script>
 <script src={{ asset('assets/js/app.js') }}></script>
